935f3ec467e0212cf8467726209b38072a3f3639
moppi <felix.riedel@make-it-mint.de>
2025-08-14 21:54:00 +0200

    added functionalities for rotating and scaling models, as well as the option to input a default directory for 3d models

--- ui.py
+++ ui.py
@@ -42,9 +42,32 @@ class MainWindow(QtWidgets.QMainWindow):
         bt_slice.setFont(BASIC_FONT_LARGE)
         bt_slice.clicked.connect(self.start_slicing)
 
+        #Transformationen
+        #scale
+        self.tb_scale = QtWidgets.QLineEdit()
+        self.tb_scale.setPlaceholderText("Skalierung des Modells in Prozent")
+        self.tb_scale.setFont(BASIC_FONT_LARGE)
+        #rotate z
+        self.tb_rotate_z = QtWidgets.QLineEdit()
+        self.tb_rotate_z.setPlaceholderText("Rotation um Z Achse in Grad")
+        self.tb_rotate_z.setFont(BASIC_FONT_LARGE)
+        #rotate y
+        self.tb_rotate_y = QtWidgets.QLineEdit()
+        self.tb_rotate_y.setPlaceholderText("Rotation um Y Achse in Grad")
+        self.tb_rotate_y.setFont(BASIC_FONT_LARGE)
+        #rotate x
+        self.tb_rotate_x = QtWidgets.QLineEdit()
+        self.tb_rotate_x.setPlaceholderText("Rotation um X Achse in Grad")
+        self.tb_rotate_x.setFont(BASIC_FONT_LARGE)
+
+        #Settings
         bt_exe_path = QtWidgets.QPushButton("Slicer Pfad")
         bt_exe_path.setFont(BASIC_FONT_LARGE)
         bt_exe_path.clicked.connect(self.set_slicer_path)
+
+        bt_stl_path = QtWidgets.QPushButton("Standard Modellordner festlegen")
+        bt_stl_path.setFont(BASIC_FONT_LARGE)
+        bt_stl_path.clicked.connect(self.set_stl_files_path)
 
         bt_close = QtWidgets.QPushButton("Beenden")
         bt_close.setFont(BASIC_FONT_LARGE)
@@ -54,6 +77,11 @@ class MainWindow(QtWidgets.QMainWindow):
         layout.addWidget(bt_filepath)
         layout.addWidget(self.txt_filepath)
         layout.addWidget(bt_slice)
+        layout.addWidget(self.tb_scale)
+        layout.addWidget(self.tb_rotate_z)
+        layout.addWidget(self.tb_rotate_y)
+        layout.addWidget(self.tb_rotate_x)
+        layout.addWidget(bt_stl_path)
         layout.addWidget(bt_exe_path)
         layout.addWidget(bt_close)
 
@@ -69,10 +97,32 @@ class MainWindow(QtWidgets.QMainWindow):
         freq = 440
         if success:
             winsound.Beep(freq, duration)
+            pass
         else:
             winsound.Beep(freq, duration)
             time.sleep(.3)
             winsound.Beep(freq, duration)
+            pass
+
+    def get_scale_value(self):
+        try:
+            scale_value = int(self.tb_scale.text())
+            return scale_value
+        except:
+            return 100
+
+    def get_rotation_value(self, axis):
+        if axis == "z":
+            widget = self.tb_rotate_z
+        elif axis == "y":
+            widget = self.tb_rotate_y
+        elif axis == "x":
+            widget = self.tb_rotate_x
+        try:
+            value = float(widget.text())
+            return value
+        except:
+            return 0
         
     def start_slicing(self):
         if self.print_file_stl == None:
@@ -80,15 +130,20 @@ class MainWindow(QtWidgets.QMainWindow):
             return False
         save_path = QtWidgets.QFileDialog.getSaveFileName(self, 'GCode Speichern',filter="(*.gcode)")
         try:
-            os.system(f"{self.get_slicer_path()} -g  {self.print_file_stl} -o {save_path[0]} --load config_files/{self.cb_profile.currentText()}")
+            os.system(f"{self.get_slicer_path()} -g  {self.print_file_stl} --scale {self.get_scale_value()}% --rotate {self.get_rotation_value("z")} --rotate-y {self.get_rotation_value("y")} --rotate-x {self.get_rotation_value("x")} -o {save_path[0]} --load config_files/{self.cb_profile.currentText()}")
             self.beep(True)
         except Exception as e:
             self.beep(False)
             print(e)
 
 
+
     def get_slicer_path(self):
         f = open("slicerpath.txt", "r")
+        return f.read()
+
+    def get_stl_model_path(self):
+        f = open("modelpath.txt", "r")
         return f.read()
     
     def refactor_path(self, path):
@@ -117,8 +172,23 @@ class MainWindow(QtWidgets.QMainWindow):
         else:
             self.beep(False)
 
+    def set_stl_files_path(self): 
+        try:
+            dialog = QtWidgets.QFileDialog.getExistingDirectory(self, "Modellordnerpfad auswählen")
+            path = dialog
+            print(path)
+            f = open("modelpath.txt", "w")
+            f.write(path)
+            f.close()
+            self.model_path = path
+            self.beep(True)
+        except Exception as e:
+            print(e)
+            self.beep(False)
+
     def select_file(self):
-        print_file = QtWidgets.QFileDialog.getOpenFileName(self, "Dateipfad auswählen", filter="3D Models (*.stl)")
+        print(self.get_stl_model_path())
+        print_file = QtWidgets.QFileDialog.getOpenFileName(self, "Dateipfad auswählen", f"{self.get_stl_model_path()}",filter="3D Models (*.stl)")
         if print_file[0].endswith(".stl"):
             self.print_file_stl = self.refactor_path(print_file[0])
             self.txt_filepath.setText(print_file[0])
